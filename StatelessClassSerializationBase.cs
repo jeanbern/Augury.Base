8460747cc8b5316c7b8df9ac3eb64f9150f41522
jeanbern <jeanbern@ualberta.ca>
2016-07-13 21:30:58 -0600

    #6 Add ability to easily serialize basic classes with no data to be persisted.

---
+++ StatelessClassSerializationBase.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Augury.Base
+{
+    public abstract class StatelessClassSerializationBase<T> : ISerializer<T>
+        where T : class, new()
+    {
+        public T Deserialize(Stream stream)
+        {
+            return new T();
+        }
+
+        public void Serialize(Stream stream, T data)
+        {
+            return;
+        }
+    }
+}
